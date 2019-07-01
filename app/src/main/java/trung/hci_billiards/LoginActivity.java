c0aad4a4d5ec6bd9e7ffa19020d542289605fb61
Hoangttse63415 <Hoangttse63415@fpt.edu.vn>
2019-07-01 10:49:03 +0700

    update login toast

--- app/src/main/java/trung/hci_billiards/LoginActivity.java
+++ app/src/main/java/trung/hci_billiards/LoginActivity.java
@@ -3,6 +3,7 @@ package trung.hci_billiards;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,5 +24,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+
+        Toast.makeText(this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
     }
 }
