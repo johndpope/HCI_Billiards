60ade373f8180f6f563e970dc21d6349a59f9d4e
thanh phan <thanhpvse62274@fpt.edu.vn>
2019-07-02 18:09:12 +0700

    DatBan time picker

--- app/src/main/java/trung/hci_billiards/DatBan.java
+++ app/src/main/java/trung/hci_billiards/DatBan.java
@@ -5,23 +5,61 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.Calendar;
+
 public class DatBan extends AppCompatActivity {
+
+    TextView textView ;
+    private static final int DATE_ID = 0;
+    private static final int TIME_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dat_ban);
 
+        textView = findViewById(R.id.txtSetDate);
+        textView.setClickable(true);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_ID);
+            }
+        });
     }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Calendar calendar = Calendar.getInstance();
+
+        int yeah = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int date = calendar.get(Calendar.DATE);
+
+        return new DatePickerDialog(DatBan.this,date_listener,yeah,month,date);
+    }
+
+
+    DatePickerDialog.OnDateSetListener date_listener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            String date1 = String.valueOf(month) + "/" + String.valueOf(dayOfMonth)
+                    + "/" + String.valueOf(year);
+            textView.setText(date1);
+        }
+    };
 
     public void clickToXemThemThongTin(View view) {
         TextView textView = findViewById(R.id.txtXemThemThongTin);
--- app/src/main/java/trung/hci_billiards/MapFragment.java
+++ app/src/main/java/trung/hci_billiards/MapFragment.java
@@ -1,6 +1,8 @@
 package trung.hci_billiards;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.FragmentActivity;
@@ -32,6 +34,17 @@ public class MapFragment extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // Add a marker in Sydney and move the camera
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
         LatLng hcm = new LatLng(10.852985, 106.629544);
         mMap.setMinZoomPreference(15);
         mMap.addMarker(new MarkerOptions().position(hcm).title("Fpt University"));
