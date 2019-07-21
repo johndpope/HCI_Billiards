aa4074d72722e4cf325ee97b36c0014a9e628cbd
thanh phan <thanhpvse62274@fpt.edu.vn>
2019-07-21 15:04:18 +0700

    Try Booking

---
+++ app/src/main/java/trung/hci_billiards/BidaLo.java
@@ -0,0 +1,14 @@
+package trung.hci_billiards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BidaLo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bida_lo);
+    }
+}
--- app/src/main/java/trung/hci_billiards/DatBan.java
+++ app/src/main/java/trung/hci_billiards/DatBan.java
@@ -2,17 +2,17 @@ package trung.hci_billiards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
+
+import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.TimePickerDialog;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.DatePicker;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -27,6 +27,11 @@ public class DatBan extends AppCompatActivity {
     int date_tang;
 
     private static final String TAG = "DatBan";
+    private static final String BIDA_BANG = "Bida Băng";
+    private static final String BIDA_LO = "Bida Lỗ";
+    String chonban ;
+    Dialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +70,37 @@ public class DatBan extends AppCompatActivity {
                 }
             }
         });
+
+
+
+        final TextView txtLoaiBan;
+        txtLoaiBan = findViewById(R.id.txtLoaiBan);
+        txtLoaiBan.setText(BIDA_BANG);
+        ImageView imgLoaiBan1, imgLoaiBan2;
+        imgLoaiBan1 = findViewById(R.id.imgLoaiBan1);
+        imgLoaiBan2 = findViewById(R.id.imgLoaiBan2);
+        imgLoaiBan1.setClickable(true);
+        imgLoaiBan1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtLoaiBan.getText().toString().equals(BIDA_BANG)){
+                    txtLoaiBan.setText(BIDA_LO);
+                }else{
+                    txtLoaiBan.setText(BIDA_BANG);
+                }
+            }
+        });
+        imgLoaiBan2.setClickable(true);
+        imgLoaiBan2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtLoaiBan.getText().toString().equals(BIDA_BANG)){
+                    txtLoaiBan.setText(BIDA_LO);
+                }else{
+                    txtLoaiBan.setText(BIDA_BANG);
+                }
+            }
+        });
 //        textView_date.setClickable(true);
 //        textView_date.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -91,54 +127,103 @@ public class DatBan extends AppCompatActivity {
         textView_Time.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "onClick: TimePicker");
-                final Calendar calendar = Calendar.getInstance();
-
-                TimePickerDialog timePickerDialog = new TimePickerDialog(DatBan.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        textView_Time.setText(hourOfDay+":"+minute);
-                    }
-                },0,0,false);
-                timePickerDialog.show();
-
-            }
-        });
-
-        textView_songuoi = findViewById(R.id.txtSoNguoi);
-        textView_songuoi.setText(""+soNguoi);
-
-        giam = findViewById(R.id.imgDes);
-        giam.setClickable(true);
-        giam.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(soNguoi<2){
-
+                String loaiBan = txtLoaiBan.getText().toString();
+                if (loaiBan.equals(BIDA_BANG)){
+                    dialog = new Dialog(DatBan.this, android.R.style.Theme_Material_NoActionBar_Fullscreen);
+                    dialog.setTitle("Đặt bàn bida lỗ");
+                    dialog.setContentView(R.layout.activity_bida_bang);
+                    dialog.show();
                 }else{
-                    soNguoi--;
-                    textView_songuoi.setText(""+soNguoi);
+                    Dialog dialog = new Dialog(DatBan.this);
+                    dialog.setTitle("Đặt bàn bida băng");
+                    dialog.setContentView(R.layout.activity_bida_lo);
+                    dialog.show();
                 }
 
             }
         });
 
-        tang = findViewById(R.id.imgAsc);
-        tang.setClickable(true);
-        tang.setOnClickListener(new View.OnClickListener() {
+
+
+        //        textView_Time.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: TimePicker");
+//                final Calendar calendar = Calendar.getInstance();
+//
+//                TimePickerDialog timePickerDialog = new TimePickerDialog(DatBan.this, new TimePickerDialog.OnTimeSetListener() {
+//                    @Override
+//                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//                        textView_Time.setText(hourOfDay+":"+minute);
+//                    }
+//                },0,0,false);
+//                timePickerDialog.show();
+//
+//            }
+//        });
+//
+//        textView_songuoi = findViewById(R.id.txtSoNguoi);
+//        textView_songuoi.setText(""+soNguoi);
+
+//        giam = findViewById(R.id.imgDes);
+//        giam.setClickable(true);
+//        giam.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(soNguoi<2){
+//
+//                }else{
+//                    soNguoi--;
+//                    textView_songuoi.setText(""+soNguoi);
+//                }
+//
+//            }
+//        });
+//
+//        tang = findViewById(R.id.imgAsc);
+//        tang.setClickable(true);
+//        tang.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                soNguoi++;
+//                textView_songuoi.setText(""+soNguoi);
+//            }
+//        });
+        Button datbanngay = findViewById(R.id.btnDatBanNgay);
+        datbanngay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                soNguoi++;
-                textView_songuoi.setText(""+soNguoi);
+//                Intent intent = new Intent(DatBan.this, BookingFragment.class);
+//                startActivity(intent);
             }
         });
     }
 
+    public void Ban1sang(View view) {
+        Button B = (Button)view;
+        chonban = "Sáng - " + B.getText().toString();
+        textView_Time.setText(chonban);
+        dialog.dismiss();
 
+    }
+
+    public void Ban2sang(View view) {
+        Button B = (Button)view;
+        chonban = "Sáng - " + B.getText().toString();
+        textView_Time.setText(chonban);
+        dialog.dismiss();
+
+    }
     public void clickToXemThemThongTin(View view) {
         TextView textView = findViewById(R.id.txtXemThemThongTin);
         Intent intent = new Intent(this,BilliardDetail.class);
         startActivity(intent);
     }
 
+    public void BanDaDuocDat(View view) {
+        Toast.makeText(DatBan.this,"Bàn này đã có người đặt\nMời bạn chọn bàn khác", Toast.LENGTH_LONG).show();
+        new AlertDialog.Builder(DatBan.this)
+                .setMessage("Bàn này đã có người đặt.\nMời bạn chọn bàn khác")
+                .setPositiveButton("OK",null);
+    }
 }
